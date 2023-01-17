06479741ca2df80ecc820e9901dd162ea945d554
barsu5489 <ebarsulai@gmail.com>
2023-01-17 23:04:34 +0300

    adding classame on click

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories}) {
+  const [selected, setSelected] = useState({});
+  function handleClick(index){
+    setSelected({...selected, [index]:!selected[index]})
+  }
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {/* render <button> elements for each category here */
+      categories.map((btn,index)=>{
+        return <button className={selected[index] ? 'selected':''} key={index} onClick={()=>handleClick(index)}>{btn}</button>
+      })
+      }
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
+
 
 function Task({text,category}) {
+
+  console.log(text)
   
-  console.log('a')
+  const [data, setData] = ([text])
+  const [info ,setInfo] = useState('task')
+  function handleClick(e){
+if(text === data){
+  console.log('equal')
+}
+    if(info==='task'){
+      setInfo('remove')
+    }
+  }
+  
   return (
-    <div className="task">
+    <div className={info}>
       <div className="label">{text}</div>
       <div className="text">{category}</div>
-      <button className="delete">X</button>
+      <button className="delete" onClick={handleClick}>X</button>
     </div>
   );
 }
